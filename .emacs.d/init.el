0c6724a41e19ed89b92d79c330049acd02efab45
pakkimz <pakkimz@gmail.com>
2021-08-14 14:22:18 +0700

    evil mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -105,7 +105,6 @@
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
 (setq web-mode-enable-auto-closing t)
-(setq web-mode-enable-current-element-highlight t)
 
 ;; Omnicomplete for html and css
 ; (require 'auto-complete-config)
