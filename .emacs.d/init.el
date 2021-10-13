a45a8eef4cce0fff036b8f172dcd769d900d8d1d
pakkimz <pakkimz@gmail.com>
2021-10-13 20:02:59 +0700

    emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -109,6 +109,7 @@
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.ejs$" . web-mode))
 (add-to-list 'load-path "~/.emacs.d/vendor/jade-mode")
