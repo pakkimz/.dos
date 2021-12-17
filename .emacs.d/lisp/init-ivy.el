2e181a4e1b1be049e7b75a2582647758293ae748
pakkimz <pakkimz@gmail.com>
2021-12-17 21:03:30 +0700

    emacs

--- .emacs.d/lisp/init-ivy.el
+++ .emacs.d/lisp/init-ivy.el
@@ -44,6 +44,7 @@
 (with-eval-after-load 'ivy
                       (global-set-key "\C-s" 'swiper)
                       (global-set-key "\M-x" 'counsel-M-x)
+                      (global-set-key (kbd "C-x C-b") 'ivy-switch-buffer)
                       (global-set-key (kbd "C-x C-f") 'counsel-find-file)
                       (global-set-key (kbd "C-x C-r") 'counsel-recentf)
                       (global-set-key (kbd "\C-w") 'counsel-up-directory))
