44541480e2ba82a2b4fed367bc67c8c13d3831c1
pakkimz <pakkimz@gmail.com>
2022-09-25 18:04:14 +0700

    ...

--- .emacs.d/lisp/init-jbeans-theme.el
+++
@@ -1,6 +0,0 @@
-(setq custom-safe-themes t)   ;; all safe, don't ask
-(use-package jbeans-theme
-             :config
-             (load-theme 'jbeans t))
-
-(provide 'init-jbeans-theme)
--- .emacs.d/lisp/init-rjsx-mode.el
+++
@@ -1,6 +0,0 @@
-(use-package rjsx-mode
-             :init
-             (add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
-             )
-
-(provide 'init-rjsx-mode)
