4c8db970a3841bc8040000ccf8535bca347151c7
pakkimz <pakkimz@gmail.com>
2022-09-28 18:42:44 +0700

    emacs

--- .emacs.d/lisp/init-lorem-ipsum.el
+++
@@ -1,8 +0,0 @@
-(use-package lorem-ipsum
-             :bind
-             ("C-c C-l s" . 'lorem-ipsum-insert-sentences)
-             ("C-c C-l p" . 'lorem-ipsum-insert-paragraphs)
-             ("C-c C-l l" . 'lorem-ipsum-insert-list)
-             )
-
-(provide 'init-lorem-ipsum)
